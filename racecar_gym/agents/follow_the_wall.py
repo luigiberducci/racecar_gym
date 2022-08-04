8b32e8c86cc93a1ce5865ef8376f0ecc192d4e1d
luigiberducci <luigi.berd@gmail.com>
2022-08-04 17:26:47 +0200

    refactoring ftg and ftw agents

--- racecar_gym/agents/follow_the_wall.py
+++ racecar_gym/agents/follow_the_wall.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, Dict
 
 import numpy as np
 
-from agents.agent import Agent, State, ObservationDict, ActionDict
+from racecar_gym.agents.agent import Agent, State, ObservationDict, ActionDict
 
 
 class PID:
@@ -13,17 +13,15 @@ class PID:
         ki: float = 0.0
         kd: float = 0.0
 
-    def __init__(self, config: PIDConfig, init_control=0):
+    def __init__(self, config: PIDConfig):
         self._c = config
-        self.init_control = init_control
         self.prev_error = 0
         self.integral = 0
 
     def control(self, target, measurement):
         error = target - measurement
         self.integral += error
-        output_control = self.init_control + \
-                         self._c.kp * error + \
+        output_control = self._c.kp * error + \
                          self._c.ki * self.integral + \
                          self._c.kd * (error - self.prev_error)
         self.prev_error = error
@@ -31,59 +29,60 @@ class PID:
 
 
 class FollowTheWall(Agent):
+    """
+    Follow The Wall Controller which keep a fixed distance to the Left wall at a fixed speed.
+    It works decently in smooth tracks (no 90 degree or hairpin curves).
+
+    Note: PID parameters *must* be tuned according to the track, target distance and target speed.
+    """
     @dataclass
     class Config:
-        # scan configuration
+        # scan configuration (used to process observation and compute angles)
         scan_field: str = "lidar"  # field in observation corresponding to the lidar scan
         scan_fov_degree: int = 270  # field-of-view of lidar sensor in degree
         scan_size: int = 1080  # field-of-view of lidar sensor in degree
-        # actuators limits
+        # actuators limits (used to normalize actions)
         max_steering: float = 0.42  # max steering (in absolute value, assuming symmetric steering)
         min_speed: float = 0.0  # min speed (m/s)
         max_speed: float = 3.5  # max speed (m/s)
+        # controller params
+        target_distance_left: float = 0.5,  # distance to the left wall (m)
+        target_speed: float = 1.0,          # constant reference velocity (ms)
+        alpha: float = 60                   # reference angle for the 2nd beam
+        n_beams_for_dist: int = 3           # used to estimate distance by averaging this nr of beams
+        pid_config: PID.PIDConfig = PID.PIDConfig(1.0, 0.0, 0.0)    # important: pid for lateral control
 
-    def __init__(self,
-                 target_distance_left: float = 0.5,
-                 target_speed: float = 1.0,
-                 reference_angle: float = 60):
+    def __init__(self, config: Dict = {}):
         # config controllers
-        self._c = self.Config()
-        self._steer_ctrl = PID(
-            PID.PIDConfig(0.8 * 16.0, 0.0, 0.1 * 0.4 * 16.0),   # tuned with ZN method (Ku=16, Tu=0.5)
-            init_control=0.0)
-
-        # target variables
-        self._target_distance_left = target_distance_left
-        self._target_speed = target_speed
+        self._c = self.Config(**config)
+        self._steer_ctrl = PID(config=self._c.pid_config)
 
         # internal params
-        self._alpha = reference_angle  # degrees
         self._n_beams_for_dist = 3  # nr averaged beams to estimate wall distance
-        self._cos_alpha = np.cos(self._alpha * np.pi / 180)
-        self._sin_alpha = np.sin(self._alpha * np.pi / 180)
+        self._cos_alpha = np.cos(self._c.alpha * np.pi / 180)
+        self._sin_alpha = np.sin(self._c.alpha * np.pi / 180)
         # reference rays
         self._left_index = self._get_beam_id_from_angle(angle_deg=-90)
         self._right_index = self._get_beam_id_from_angle(angle_deg=90)
-        self._alpha_index = self._get_beam_id_from_angle(angle_deg=-self._alpha)  # second ray: alpha angle
+        self._alpha_index = self._get_beam_id_from_angle(angle_deg=-self._c.alpha)  # second ray: alpha angle
 
     def _get_beam_id_from_angle(self, angle_deg: int):
         assert - self._c.scan_fov_degree / 2 <= angle_deg <= self._c.scan_fov_degree / 2, f"invalid angle {angle_deg}"
         angles = np.linspace(0, self._c.scan_fov_degree, self._c.scan_size)
-        differences = (angles - (self._c.scan_fov_degree / 2 + angle_deg))**2
+        differences = (angles - (self._c.scan_fov_degree / 2 + angle_deg)) ** 2
         return np.argmin(differences)
 
     def get_action(self, observation: ObservationDict,
                    state: State = None,
                    return_norm_actions: bool = True) -> Tuple[ActionDict, State]:
-        assert "velocity" in observation and "lidar" in observation, f"unvalid observation, keys: {observation.keys()}"
+        assert self._c.scan_field in observation, f"unvalid observation, keys: {observation.keys()}"
         # process observation
-        current_speed = observation["velocity"][0]
-        scan = observation["lidar"]
+        scan = observation[self._c.scan_field]
 
         # compute speed, steering targets
         predicted_distance = self._compute_distance(scan)
-        steering = self._steer_ctrl.control(self._target_distance_left, predicted_distance)
-        speed = self._target_speed
+        steering = self._steer_ctrl.control(self._c.target_distance_left, predicted_distance)
+        speed = self._c.target_speed
 
         # convert to normalized scale
         if return_norm_actions:
