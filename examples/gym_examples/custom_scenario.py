3db5edf7f42bf1ff8008add9fda44e12e7fa89e9
Luigi Berducci <luigi.berd@gmail.com>
2022-09-13 14:42:26 +0200

    update custom scenario with ftg agent

--- examples/gym_examples/custom_scenario.py
+++ examples/gym_examples/custom_scenario.py
@@ -2,8 +2,8 @@ from time import sleep
 
 import numpy as np
 
+from racecar_gym.agents.follow_the_gap import FollowTheGap
 from racecar_gym import MultiAgentScenario
-from racecar_gym.agents import FollowTheGap
 from racecar_gym.envs import gym_api
 
 scenario = MultiAgentScenario.from_spec(
@@ -16,23 +16,27 @@ env = gym_api.MultiAgentRaceEnv(scenario=scenario)
 print(env.observation_space)
 print(env.action_space)
 
-done = False
-obs = env.reset(mode='random_ball')
 ftg = FollowTheGap()
-_ = ftg.reset()
 
-maxv = 0
-while not done:
-    agent_action, _ = ftg.get_action(observation=obs["A"])
-    action = env.action_space.sample()
-    action["A"] = agent_action
+for episode in range(10):
+    print(episode)
+    done = False
+    obs = env.reset(mode='grid')
+    _ = ftg.reset({
+        "gap_threshold": 1.5,
+        "base_speed": 1.25 + np.random.rand() * 0.5,
+        "variable_speed": 0.25 + np.random.rand() * 0.5,
+    })
 
-    obs, rewards, dones, states = env.step(action)
+    while not done:
+        agent_action, _ = ftg.get_action(observation=obs["A"])
 
-    maxv = max(maxv, obs["A"]["velocity"][0])
-    print(maxv)
+        action = {"A": ftg.get_action(observation=obs["A"])[0],
+                  "B": ftg.get_action(observation=obs["B"])[0]}
 
-    done = any(dones.values())
-    sleep(0.01)
+        obs, rewards, dones, states = env.step(action)
+        print(states["A"]["progress"])
+        done = any(dones.values())
+        sleep(0.01)
 
 env.close()
